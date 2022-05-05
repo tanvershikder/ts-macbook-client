fdb6ce7da3b95059d265b47ea9373aef36433590
Your Name <you@example.com>
2022-05-05 16:45:43 +0600

    env done

--- src/firebase.init.js
+++ src/firebase.init.js
@@ -6,12 +6,12 @@ import {getAuth} from 'firebase/auth'
 
 // Your web app's Firebase configuration
 const firebaseConfig = {
-  apiKey: "AIzaSyCybngroW-qlJ5dmP0eRnkFxZRvvLCWXtg",
-  authDomain: "ts-mackbook.firebaseapp.com",
-  projectId: "ts-mackbook",
-  storageBucket: "ts-mackbook.appspot.com",
-  messagingSenderId: "27330722175",
-  appId: "1:27330722175:web:bd39decde7380fe54fc24a"
+  apiKey:process.env.REACT_APP_apiKey,
+  authDomain:process.env.REACT_APP_authDomain,
+  projectId:process.env.REACT_APP_projectId,
+  storageBucket:process.env.REACT_APP_storageBucket,
+  messagingSenderId:process.env.REACT_APP_messagingSenderId,
+  appId:process.env.REACT_APP_appId
 };
 
 // Initialize Firebase
